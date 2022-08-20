a458b8f3ef94b8ecaee4aa50ff39ffa226f8e292
bimaap <let.bimaap@gmail.com>
2022-08-20 20:33:20 +0700

    feat: checkout

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 //Penggunaan route silahkan sesuaikan denggan kebutuhan
 router.use('/',require('./chats'));
+router.use("/checkouts", require("./checkouts"));
 
 module.exports=router
