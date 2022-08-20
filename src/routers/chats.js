edeac91ba6a93b713c85dd40151a1c6cf6dd6ff0
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-08-20 10:31:32 +0700

    fix: router chats

--- src/routers/chats.js
+++ src/routers/chats.js
@@ -3,8 +3,8 @@ const chatsController = require('../controllers/chats');
 const auth = require('../middleware/auth')
 
 chats.post('/chats',auth,chatsController.wrapperChat);
-chats.post('/chat/content',chatsController.contentChat);
+chats.post('/chat/content',auth,chatsController.contentChat);
 chats.get('/chat',auth,chatsController.getAllWrapper);
-chats.get('/chat/content',chatsController.getAllChat);
+chats.get('/chat/content',auth,chatsController.getAllChat);
 
 module.exports=chats
