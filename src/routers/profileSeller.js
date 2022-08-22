e5d6e3a0c6a6ba598142ccaf00b15489573c217f
triharyoibrahim <rizkitriharyoibrahim@gmail.com>
2022-08-22 22:35:53 +0700

    fix upload image cloudinary

--- src/routers/profileSeller.js
+++ src/routers/profileSeller.js
@@ -1,28 +1,23 @@
 const router = require('express').Router();
-
+const { body } = require("express-validator");
 const profileSellerController = require("../controllers/profileSeller");
+const uploadMiddleware = require('../middleware/upload');
 
-router.get(
-    "/profile/seller",  
-    profileSellerController.getAllSeller
-  );
-router.get(
-   "/profile/seller/:id",
+const validation = [
+  body("phone_num")
+    .isNumeric()
+    .withMessage("Please input number only"),
+  body("full_name")
+    .isLength({ min: 2 })
+    .withMessage("Please input your fullname correctly"),
+];
 
-    profileSellerController.getSellerById
-  );
-
-router.patch(
-    "/profile/seller/:id", profileSellerController.updateSeller
-  );
+router.get("/profile/seller",  profileSellerController.getAllSeller  );
+router.get("/profile/seller/:id",profileSellerController.getSellerById);
+router.patch("/profile/seller/:id", uploadMiddleware, profileSellerController.updateSeller);
 
 //Admin
-router.post(
-    "/profile/admin/seller/:id", profileSellerController.createSeller
-  );
-router.delete(
-    "/profile/admin/seller/:id",
-    profileSellerController.deleteSeller
-  );
+router.post("/profile/admin/seller/", uploadMiddleware, profileSellerController.createSeller);
+router.delete("/profile/admin/seller/:id",profileSellerController.deleteSeller);
   
-  module.exports = router;
+module.exports = router;
